56edc1feb8e433a6940d36c0a455a542bd553461
devynnpalmer <palmerdevynn@gmail.com>
2018-06-14 19:05:01 -0400

    established connection

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWallerProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'advance welcome today exile six diary double scare bag tornado fame father',
+    'https://rinkeby.infura.io/8DN0ciDeaEamsLe9O7MS'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi!']})
+        .send({ gas: 1000000, from: accounts[0]});
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
